fefb866dc0995b02c60715152ebbd20b603891fb
Skye Brown <skye.brownh@gmail.com>
2020-02-07 12:51:54 -0500

    populate geojson with db data

--- db/db.js
+++ db/db.js
@@ -9,7 +9,45 @@ mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: t
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+const geojson = {
+    "type": "FeatureCollection",
+    "features": []
+};
 db.once('open', () => {
     // TODO: query saved documents to show on map
+    // {
+    //     "type": "Feature",
+    //     "properties": {
+    //     "message": "Hello, AInc!",
+    //     "iconSize": [45, 45]
+    //     },
+    //     "geometry": {
+    //     "type": "Point",
+    //     "coordinates": [-85.7585, 38.2527]
+    //     }
+    // }
+    
+    // query all Members from db to show on map
+    Member
+        .find({})
+        .exec((err, res) => {
+            if (err) return console.error(err);
+            res.forEach(member => {
+                const memberObj = member.toObject();
+                geojson.features.push({
+                    type: 'Feature',
+                    properties: {
+                        name: `${memberObj.first_name} ${memberObj.last_name}`,
+                        iconSize: [45, 45]
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [memberObj.latitude, memberObj.longitude]
+                    }
+                });
+            });
+            // console.log(geojson.features.length);
+            db.close();
+        });
 });
-
