8f79f83648d38a6625bbb5bc3ad7caec14db6e82
Skye Brown <skye.brownh@gmail.com>
2020-03-27 17:10:38 -0400

    import mapboxgl-spiderifier vendor file

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // static files
+app.use(express.static(path.join(__dirname, 'vendors')));
 app.use(express.static(path.join(__dirname, 'styles')));
 app.use(express.static(path.join(__dirname, 'images')));
 
